eb01229b5f44978e962a4f80cc2afa8cb3b31d63
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2025-02-07 06:39:22 +0900

    캐릭터 인풋 기본설정

--- Source/DDS/PlayerController/InGamePlayerController.cpp
+++ Source/DDS/PlayerController/InGamePlayerController.cpp
@@ -2,4 +2,68 @@
 
 
 #include "InGamePlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "DDS/Character/CombatComponent/CombatComponent.h"
+#include "DDS/ETC/CustomLog.h"
 
+AInGamePlayerController::AInGamePlayerController()
+{
+	CombatComponent = CreateDefaultSubobject<UCombatComponent>(TEXT("Combat Component"));
+}
+
+void AInGamePlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void AInGamePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if(!Subsystem) return;
+
+	Subsystem->AddMappingContext(InputMappingContext, 0);
+}
+
+void AInGamePlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if(!EnhancedInputComponent) return;
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Jump);
+	EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Triggered, this, &ThisClass::Attack);
+	EnhancedInputComponent->BindAction(DashAction, ETriggerEvent::Triggered, this, &ThisClass::Dash);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Look);
+
+	MY_LOG(LogTemp, Log, TEXT("Setup Input Complete"));
+}
+
+void AInGamePlayerController::Move(const FInputActionValue& Value)
+{
+	MY_LOG(LogTemp, Log, TEXT("Character Move"));
+}
+
+void AInGamePlayerController::Jump()
+{
+	MY_LOG(LogTemp, Log, TEXT("Character Jump"));
+}
+
+void AInGamePlayerController::Attack()
+{
+	MY_LOG(LogTemp, Log, TEXT("Character Attack"));
+}
+
+void AInGamePlayerController::Dash()
+{
+	MY_LOG(LogTemp, Log, TEXT("Character Dash"));
+}
+
+void AInGamePlayerController::Look(const FInputActionValue& Value)
+{
+	MY_LOG(LogTemp, Log, TEXT("Character Look"));
+}
--- Source/DDS/PlayerController/InGamePlayerController.h
+++ Source/DDS/PlayerController/InGamePlayerController.h
@@ -4,8 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "InputMappingContext.h"
 #include "InGamePlayerController.generated.h"
 
+class UCombatComponent;
 /**
  * 
  */
@@ -13,5 +15,48 @@ UCLASS()
 class DDS_API AInGamePlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AInGamePlayerController();
+
+protected:
+	virtual void Tick(float DeltaSeconds) override;
 	
+	virtual void BeginPlay() override;
+	
+	virtual void SetupInputComponent() override;
+
+	// --- Input Action Delegate Function ---
+	UFUNCTION(BlueprintCallable)
+	void Move(const FInputActionValue& Value);
+
+	UFUNCTION(BlueprintCallable)
+	void Jump();
+
+	UFUNCTION(BlueprintCallable)
+	void Attack();
+
+	UFUNCTION(BlueprintCallable)
+	void Dash();
+
+	UFUNCTION(BlueprintCallable)
+	void Look(const FInputActionValue& Value);
+	
+public:
+	UPROPERTY()
+	TObjectPtr<UCombatComponent> CombatComponent;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> AttackAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> JumpAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> DashAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
 };
