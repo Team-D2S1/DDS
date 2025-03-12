8e11e3dd46a482311c6dab3b6cd0d7af63b9eba7
machamy <macham2001@gmail.com>
2025-03-13 06:18:44 +0900

    feat 화면 띄우는 커스텀 로그

--- Source/DDS/ETC/CustomLog.h
+++ Source/DDS/ETC/CustomLog.h
@@ -6,3 +6,26 @@
 // 커스텀 로그 - 나중에 로그 지우기 쉽게, 어디서 실행됐는지 알기 편하게
 #define MY_LOG(Category, Verbosity, Format, ...) \
 UE_LOG(Category, Verbosity, TEXT("[%s - %d] : ") Format, ANSI_TO_TCHAR(__FUNCTION__), __LINE__, ##__VA_ARGS__)
+
+
+namespace Debug
+{
+	// 로그 타입(ELogVerbosity)과 메시지, 색
+	static void Log(const FString& Message,const FColor& Color = FColor::MakeRandomColor())
+	{
+		MY_LOG(LogTemp, Type::Log, "%s", *Message);
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, Color, Message);
+		}
+	}
+
+	static void LogError(const FString& Message,const FColor& Color = FColor::MakeRandomColor())
+	{
+		MY_LOG(LogTemp, Type::Error, "%s", *Message);
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, Color, Message);
+		}
+	}
+}
