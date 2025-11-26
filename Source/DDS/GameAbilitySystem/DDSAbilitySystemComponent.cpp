eaa542e9fc49b5a2632714e42ad5b6cd9f884b6e
macham <68574956+machamy@users.noreply.github.com>
2025-11-26 23:59:33 +0900

    feat : 레벨업/경험치 획득시 안내메시지

--- Source/DDS/GameAbilitySystem/DDSAbilitySystemComponent.cpp
+++ Source/DDS/GameAbilitySystem/DDSAbilitySystemComponent.cpp
@@ -96,8 +96,8 @@ void UDDSAbilitySystemComponent::GrantPlayerWeaponAbilities(const TArray<FDDSPla
 		AbilitySpec.Level = ApplyLevel;
 		OutHandles.AddUnique( GiveAbility(AbilitySpec));
 		// MY_LOG(LogTemp, Log, TEXT("Granting %s, tag: %s"), *AbilitySet.AbilityToGrant->GetName(), *AbilitySet.InputTag.ToString());
-		bool isServer = GetOwner()->HasAuthority();
-		MY_LOG_DISPLAY_NET(isServer, TEXT("Granting %s, tag: %s"), *AbilitySet.AbilityToGrant->GetName(), *AbilitySet.InputTag.ToString());
+		// bool isServer = GetOwner()->HasAuthority();
+		// MY_LOG_DISPLAY_NET(isServer, TEXT("Granting %s, tag: %s"), *AbilitySet.AbilityToGrant->GetName(), *AbilitySet.InputTag.ToString());
 	}
 }
 
